293ae48774c20e725b2c5d8dfd5f180be7b02add
Alan-HP <Alan-HP@DESKTOP-ALAN>
2025-05-06 08:18:42 +0530

    Samplecommitted

---
+++ JavaMavanProject/src/main/java/samplaPackage/Firstclass.java
@@ -0,0 +1,17 @@
+package samplaPackage;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		Firstclass.add();
+
+	}
+	public static void add()
+	{
+	int a=10;
+	int b=20;
+    int sum=a+b;
+  System.out.println(sum); 
+	}
+
+}
---
+++ JavaMavanProject/src/main/java/samplaPackage/Maethodsubtract.java
@@ -0,0 +1,15 @@
+package samplaPackage;
+
+public class Maethodsubtract {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public int sub( int a,int b)
+	{
+		int difference=a-b;
+		return difference;
+	}
+	
+}
